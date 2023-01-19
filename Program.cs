f003e445c8e1865eb6ea10cb5549814c734d64d7
scrviy <91909966+scrviy@users.noreply.github.com>
2023-01-19 20:15:44 -0300

    calculo de media

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace firstProgram
+{
+    public class firstProgram
+    {
+        static void Main(string[] args)
+        {
+            double x, y, media;
+
+            Console.WriteLine("Digite o primeiro número: ");
+            x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número: ");
+            y = double.Parse(Console.ReadLine());
+
+            media = (x + y) / 2.0;
+            Console.WriteLine("Média = " + media);
+        }
+    }
+}
